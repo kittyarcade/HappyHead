79b5c70f3e4fc4f9eebf5074da6adb9e6d052130
Amanda Jopp <aclaire488@gmail.com>
2017-01-14 11:49:06 -0600

    shell created

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-var userRouter = require('./router/pets');
+var userRouter = require('./router/route1');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
@@ -10,16 +10,16 @@ app.use( express.static( "public" ));
 app.use('/router', userRouter);
 
 //database connection
-var mongoURI = "mongodb://localhost:27017/petDB";
-var MongoDB = mongoose.connect(mongoURI).connection;
-
-MongoDB.on('error', function (err) {
-    console.log('mongodb connection error:', err);
-});
-
-MongoDB.once('open', function () {
-  console.log('mongodb connection open!');
-});
+// var mongoURI = "mongodb://localhost:27017/happyhead";
+// var MongoDB = mongoose.connect(mongoURI).connection;
+//
+// MongoDB.on('error', function (err) {
+//     console.log('mongodb connection error:', err);
+// });
+//
+// MongoDB.once('open', function () {
+//   console.log('mongodb connection open!');
+// });
 
 //server
 app.listen('3000', function(){
